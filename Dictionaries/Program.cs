430b9db57817bfbcc0d9837ca185a576003087da
Your Name <you@example.com>
2017-07-12 15:32:44 -0400

    finished dictionaries class practice

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -36,7 +36,77 @@ namespace Dictionaries
             //    Console.WriteLine(drink.Value);
             //}
 
-            //
+            //Theater coat check  with 10 elements
+            //Key = Number, Value = CoatStyle
+            //Print elements to the console
+            //Dictionary<int, string> coatCheck = new Dictionary<int, string>
+            //{
+            //    {1,"Leather Jacket" }, {2,"Trench Coat" }, {3,"Baseball Jacket" }, {4, "Windbreaker" }, {5,"Hoodie" },
+            //    {6,"Bomber Jacket" }, {7,"Blue Trench Coat" }, {8,"Red Baseball Jacket" }, {9, "Yellow Windbreaker" }, {10,"Gray Hoodie" }
+            //};
+            //foreach (KeyValuePair<int,string> coat in coatCheck)
+            //{
+            //    Console.WriteLine(coat.Key);
+            //    Console.WriteLine(coat.Value);
+            //}
+
+            ////Car valet with 10 cars
+            ////key = last name, value = car name
+            ////Print elements to the console
+            //Dictionary<string, string> valet = new Dictionary<string, string>
+            //{
+            //    { "anderson","volvo"}, { "jones", "bmw"}, { "alvarado","corvette"}, {"james","hyundai" }, {"rivera","beetle" },
+            //     { "rodriguez","jaguar"}, { "hallmann", "subaru"}, { "alvarez","honda"}, {"morales","ford" }, {"rivers","jeep" }
+            //};
+            //foreach (KeyValuePair<string,string> car in valet)
+            //{
+            //    Console.WriteLine(valet.Keys);
+            //    Console.WriteLine(valet.Values);
+            //}
+
+
+            //Zoo with 10 animals
+            //key = animal type, value = number of animals
+            //Print elements with the highest quantity to the console
+            //Remove the animal with the lowest quantity
+            Dictionary<string, int> zoo = new Dictionary<string, int>()
+            {
+                {"polar bear", 3 }, {"monkey",8 }, {"iguana", 15 }, {"brown bear", 4 }, {"hippo",1 },
+                { "wolf",8 }, { "elephant", 4}, { "giraffe", 5}, { "camel", 8}, {"lion",6 }
+            };
+            foreach (KeyValuePair<string, int> animal in zoo)
+            {
+                Console.WriteLine(animal.Key + ": " + animal.Value);
+            }
+            int high = 0;
+            string zooAnimal = "";
+            int low = 100;
+            string removeAnimal = "";
+            foreach (KeyValuePair<string,int> animal in zoo)
+            {
+                if(animal.Value > high)
+                {
+                    high = animal.Value;
+                    zooAnimal = animal.Key;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(zooAnimal + ": " + high);
+            Console.WriteLine();
+
+            foreach(KeyValuePair<string, int> animal in zoo)
+            {
+                if(animal.Value <low)
+                {
+                    low = animal.Value;
+                    removeAnimal = animal.Key;
+                }
+            }
+            zoo.Remove(removeAnimal);
+            foreach (KeyValuePair<string, int> animal in zoo)
+            {
+                Console.WriteLine(animal.Key + ": " + animal.Value);
+            }
         }
     }
 }
